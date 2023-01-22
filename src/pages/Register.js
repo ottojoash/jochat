9d2c7502eb04249927eca1d33562017418d2d8a1
ottojoash <louisjoshbricks@gmail.com>
2023-01-22 14:24:13 +0300

    reg

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -4,12 +4,16 @@ const Register = () => {
   return (
     <div className="formContainer">
         <div className="formWrapper">
+            <span className="logo">jochat</span>
+            <span className="title">Register</span>
             <form>
                 <input type={text} placeholder="display name"/>
                 <input type={email} placeholder="email"/>
                 <input type={password} placeholder="password"/>
                 <input type={file}/>
+                <button>Sign up</button>
             </form>
+            <p>You do have an account? Login</p>
         </div>
         hello world
     </div>
