3d15d912f4c18372f4de7763064655a12bc187a7
ottojoash <louisjoshbricks@gmail.com>
2023-01-22 16:09:10 +0300

    side

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
-
+import Sidebar from "../components/Sidebar";
+import Chat from "../components/Chat"
+import "../style.scss";
 const Home = () =>{
     return(
         <div className="home">
             <div className="container">
-                
+             <Sidebar/>
+             <Chat/>   
             </div>
         </div>
     )
