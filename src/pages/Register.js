51c085479e787b6825ef115baf9aa533f1b86749
ottojoash <louisjoshbricks@gmail.com>
2023-01-22 14:20:15 +0300

    reg

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,7 +3,14 @@ import React from "react";
 const Register = () => {
   return (
     <div className="formContainer">
-        <div className="formWrapper"></div>
+        <div className="formWrapper">
+            <form>
+                <input type={text} placeholder="display name"/>
+                <input type={email} placeholder="email"/>
+                <input type={password} placeholder="password"/>
+                <input type={file}/>
+            </form>
+        </div>
         hello world
     </div>
   );
