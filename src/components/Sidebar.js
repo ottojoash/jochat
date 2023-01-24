42d45c28bfe63d351e92f57849a46fc8df11561d
ottojoash <louisjoshbricks@gmail.com>
2023-01-24 09:04:08 +0300

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,16 @@
 import React from "react";
-import Navbar from "../components/Navbar"
+import Navbar from "./Navbar"
+import Search from "./Search"
+import Chats from "./Chats"
 
 const Sidebar = () => {
-    return (
-        <div className="chat">
-            <Navbar/>
-        </div>
-    )
-}
+  return (
+    <div className="sidebar">
+      <Navbar />
+      <Search/>
+      <Chats/>
+    </div>
+  );
+};
 
 export default Sidebar;
