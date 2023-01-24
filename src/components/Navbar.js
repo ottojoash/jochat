ad38c9c8d8e53445f17c343ff1d38f0baa339e1a
ottojoash <louisjoshbricks@gmail.com>
2023-01-24 09:03:32 +0300

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useContext } from 'react'
+import {signOut} from "firebase/auth"
+import { auth } from '../firebase'
+import { AuthContext } from '../context/AuthContext'
 
 const Navbar = () => {
-    return (
-        <div className="navbar">
-            <span className="logo">Jochat</span>
-            <div className="user">
-               <img src="" alt=""/> 
-            </div>
-        </div>
-    )
+  const {currentUser} = useContext(AuthContext)
+
+  return (
+    <div className='navbar'>
+      <span className="logo">Lama Chat</span>
+      <div className="user">
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
+        <button onClick={()=>signOut(auth)}>logout</button>
+      </div>
+    </div>
+  )
 }
 
-export default Navbar;
+export default Navbar
