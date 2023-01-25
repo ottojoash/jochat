4290172486e1cc39a6f8e7da4bfb3911b5727c97
ottojoash <louisjoshbricks@gmail.com>
2023-01-25 13:50:53 +0300

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,28 @@
 import { initializeApp } from "firebase/app";
+
 import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: "chat-ab746.firebaseapp.com",
-  projectId: "chat-ab746",
-  storageBucket: "chat-ab746.appspot.com",
-  messagingSenderId: "901216368405",
-  appId: "1:901216368405:web:8ec942ee51611df5c49b1c",
+
+  apiKey: "AIzaSyCdor8vBVac6DBC0E9s_MxjooPNUyE8CWs",
+
+  authDomain: "chat-33449.firebaseapp.com",
+
+  projectId: "chat-33449",
+
+  storageBucket: "chat-33449.appspot.com",
+
+  messagingSenderId: "975561594325",
+
+  appId: "1:975561594325:web:70ff8b15a4563222fd195c",
+
+  measurementId: "G-G03R49D8B4"
+
 };
 
+
 // Initialize Firebase
+
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth();
-export const storage = getStorage();
-export const db = getFirestore()
+
+export const auth = getAuth(app);
