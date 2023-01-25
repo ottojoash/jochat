01619ea80b14c99eb5ff2126eae5071ee7fffdba
ottojoash <louisjoshbricks@gmail.com>
2023-01-25 14:52:20 +0300

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
 
@@ -28,3 +28,4 @@ export const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const storage = getStorage();
+export const db = getFirestore();
