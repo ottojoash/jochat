c7ed5178cdf38f0ce0308e0d0b56e9beb936d92c
ottojoash <louisjoshbricks@gmail.com>
2023-01-25 14:48:27 +0300

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-import { getStorage } from "firebase/storage";
+
 
 const firebaseConfig = {
 
